62dffd62fedb07c33a82faa96fe35608fd78b222
Mantas Šilanskas <m.silanskas@yahoo.com>
2017-12-06 16:34:41 +0200

    Add project files.

---
+++ Mello Note/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Mello_Note
+{
+    [Activity(Label = "Mello_Note", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
